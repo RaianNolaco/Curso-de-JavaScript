1fbcccd55df9acf8249c483b55d35bdfa392adf5
RaianNolaco <81272272+RaianNolaco@users.noreply.github.com>
2021-04-12 12:10:28 -0300

    Update script.js

--- Aula exercicio 4/script.js
+++ Aula exercicio 4/script.js
@@ -10,44 +10,47 @@ function contar()
 
   if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0)
     {
-        window.alert('[ERRO] Preencha todos os campos')      
+        res.innerHTML = 'Impossivel contar....'
+        window.alert('[ERRO] Preencha todos os campos') 
+        stop()     
     }
-    else if(Number(passo.value) <= 0)
+    else 
     {
-        window.alert('[ERRO] O campo passo não pode ser menor que 1, passo será considerado com valor 1')
-    }
+        res.innerHTML = 'Contando: '
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
     
-    res.innerHTML = 'Contando: '
-    var i = Number(inicio.value)
-    var f = Number(fim.value)
-    var p = Number(passo.value)
-
-    if(p >= 0)
-    {
-        p = 1
-        passo.textContent = '1'
-    }
-    
-    if( i > f)
-    {
-        //Contagem na ordem decrecente 
-        while(i >= f)
-        {   
-           res.innerHTML += i + `\u{1F449}`     
-           i = i - p
-           
+        if(p <= 0)
+        {
+            window.alert('[ERRO] O campo passo não pode ser menor que 1, passo será considerado com valor 1')
+            p = 1
         }
         
-    }
-    else
-    {
-        //Contagem na ordem crescente
-        while(i <= f)
-        {   
-           res.innerHTML += i + `\u{1F449}`     
-           i = i + p
-           
+        if( i > f)
+        {
+            //Contagem na ordem decrecente 
+            while(i >= f)
+            {   
+               res.innerHTML += i + `\u{1F449}`     
+               i = i - p
+               
+            }
+            
         }
+        else
+        {
+            //Contagem na ordem crescente
+            while(i <= f)
+            {   
+               res.innerHTML += i + `\u{1F449}`     
+               i = i + p
+               
+            }
+        }
+        res.innerHTML += `\u{1F6A9}`
+
     }
-    res.innerHTML += `\u{1F6A9}`
+    
+
 }
